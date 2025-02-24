15b030ca361449a54b0eb6ad1187d240ab03a4d5
zjk <jinkang.zhu@qq.com>
2025-02-24 23:52:22 +0800

    原始版本1.1

--- Blog/src/main/java/com/blog/build/RequestManagerBuilder.java
+++ Blog/src/main/java/com/blog/build/RequestManagerBuilder.java
@@ -18,18 +18,8 @@ public class RequestManagerBuilder {
 
     public RequestManagerBuilder buildUser(Claims claims) {
 
-        User user = new User();
-        user.setId(claims.get("id", String.class));
-        user.setUsername(claims.get("username", String.class));
-        user.setPassword(claims.get("password", String.class));
-        user.setEmail(claims.get("email", String.class));
-        user.setPhone(claims.get("phone", String.class));
-        user.setStatus(claims.get("status", String.class));
-        user.setCreateBy(claims.get("createBy", String.class));
-        user.setCreateDate(claims.get("createDate", LocalDateTime.class));
-        user.setUpdateBy(claims.get("updateBy", String.class));
-        user.setUpdateDate(claims.get("updateDate", LocalDateTime.class));
-        user.setDeleteFlag(claims.get("deleteFlag", String.class));
+        User user = (User) claims.get("user");
+
         this.requestContextManager.setUserInfo(user);
         return this;
     }

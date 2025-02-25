9dabce874bc5069272034a243c28b3eccd4fbc3a
zjk <jinkang.zhu@qq.com>
2025-02-25 20:30:19 +0800

    解决完短ttl、类型转换错误的问题

--- Blog/src/main/java/com/blog/build/RequestManagerBuilder.java
+++ Blog/src/main/java/com/blog/build/RequestManagerBuilder.java
@@ -1,11 +1,16 @@
 package com.blog.build;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.blog.context.RequestContext;
 import com.blog.pojo.User;
 import com.blog.vo.RequestContextManager;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 
 import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
 
 
 public class RequestManagerBuilder {
@@ -16,10 +21,21 @@ public class RequestManagerBuilder {
         this.requestContextManager = new RequestContextManager();
     }
 
-    public RequestManagerBuilder buildUser(Claims claims) {
-
-        User user = (User) claims.get("user");
-
+    public RequestManagerBuilder buildUser(Claims claims) throws JsonProcessingException {
+        LinkedHashMap<String, Object> userMap = (LinkedHashMap<String, Object>) claims.get("user");
+        System.out.println(JSON.toJSONString(userMap));
+        User user = new User();
+        user.setId((String) userMap.get("id"));
+        user.setUsername((String) userMap.get("username"));
+        user.setPassword((String) userMap.get("password"));
+        user.setEmail((String) userMap.get("email"));
+        user.setPhone((String) userMap.get("phone"));
+        user.setStatus((String) userMap.get("status"));
+        user.setCreateBy((String) userMap.get("createBy"));
+        user.setCreateDate((LocalDateTime) userMap.get("createDate"));
+        user.setUpdateBy((String) userMap.get("updateBy"));
+        user.setUpdateDate((LocalDateTime) userMap.get("updateDate"));
+        user.setDeleteFlag((String) userMap.get("deleteFlag"));
         this.requestContextManager.setUserInfo(user);
         return this;
     }
